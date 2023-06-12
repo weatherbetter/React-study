6852f7f02aad4e0725062937461c033cd0fe17b4
weatherbetter12@gmail.com <weatherbetter12@gmail.com>
2023-06-12 09:55:16 +0900

    feature: 파일서버 데이터서버 다를 경우

--- src/comment/CommentList.js
+++ src/comment/CommentList.js
@@ -4,27 +4,44 @@ const comments = [
     {
         name: "홍길동",
         comment: "안녕하세요. 홍길동입니다.",
-        image: "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-business-men-icon-png-image_4186858.jpg",
     },
     {
         name: "고길동",
         comment: "돌리가 싫어요.",
-        image: "https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-avatar-icon-png-image_4162757.jpg",
     },
     {
         name: "홍길동",
         comment: "호부호형이 꿈입니다.",
-        image: "https://png.pngtree.com/png-clipart/20190520/original/pngtree-male-worker-icon-graphic-png-image_3668949.jpg",
     },
 ];
+
+const users = [
+    {
+        name: "홍길동",
+        image: "https://png.pngtree.com/png-clipart/20190705/original/pngtree-vector-business-men-icon-png-image_4186858.jpg",
+    },
+    {
+        name: "고길동",
+        image: "https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-avatar-icon-png-image_4162757.jpg",
+    },
+];
+
+const commentsAndUsers = comments.map((c) => ({
+    ...c,
+    image: users.filter((u) => u.name === c.name)[0].image,
+}));
 
 function CommentList(props) {
     return (
         <>
-            {comments
-                .map((c, index) => (
-                    <Comment key={index} name={c.name} comment={c.comment} image = {c.image}/>
-                ))}
+            {commentsAndUsers.map((c, index) => (
+                <Comment
+                    key={index}
+                    name={c.name}
+                    comment={c.comment}
+                    image={c.image}
+                />
+            ))}
         </>
     );
 }
